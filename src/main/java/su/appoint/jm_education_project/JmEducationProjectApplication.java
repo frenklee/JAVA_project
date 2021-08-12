0834208cb1ce9156735c34281b38cb85da5e4ec3
Alexander-Bolotov <37707228+Alexander-Bolotov@users.noreply.github.com>
2021-08-12 14:57:17 +0300

    start commit

---
+++ src/main/java/su/appoint/jm_education_project/JmEducationProjectApplication.java
@@ -0,0 +1,13 @@
+package su.appoint.jm_education_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmEducationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmEducationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/su/appoint/jm_education_project/JmEducationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package su.appoint.jm_education_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmEducationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
