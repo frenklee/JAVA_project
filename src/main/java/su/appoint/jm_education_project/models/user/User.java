db8fed31640b6b2e08c40d3efc12ef71f75f4afc
ггггггг <heyjaf13@gmaail.com>
2021-08-23 17:44:56 +0300

    Убрал все лишнее.

--- src/main/java/su/appoint/jm_education_project/models/user/User.java
+++ src/main/java/su/appoint/jm_education_project/models/user/User.java
@@ -1,11 +1,11 @@
 package su.appoint.jm_education_project.models.user;
-import com.sun.istack.NotNull;
 import lombok.*;
 import org.springframework.validation.annotation.Validated;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -15,36 +15,29 @@ import java.util.Objects;
 @Table(name = "users")
 @Getter
 @Setter
-@Validated
 @AllArgsConstructor
 @Data
+@Builder
 public class User  {
-
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "User_generator")
     @SequenceGenerator(sequenceName = "User_sequence", name = "User_generator", initialValue = 1000)
     private Long id;
-
     @Email
     private String email;
-    private String phone;
 
+    private String phone;
     @NotBlank
     private String firstName;
     private String lastName;
-
     @NotNull
     private String password;
     private LocalDate dateOfBirth;
     private boolean is_enabled = true;
-    @NotNull
-    @OneToOne
+    @OneToOne(fetch = FetchType.EAGER)
     private Role roles;
-
     private LocalDateTime registrationDate;
     private String imageAvatar;
-
-    private String localeTag="ru";
+    private String localeTag = "ru" ;
     private Gender gender;
-
 }
